b0731d02e660c0adecfae7bc05c03ceca372ef42
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-05-17 17:17:54 +0300

    CompanyRules Locale and MessageSource instances are removed

--- src/main/java/com/bzncrsrtc/kurumsalmuvekkil/rules/CompanyRules.java
+++ src/main/java/com/bzncrsrtc/kurumsalmuvekkil/rules/CompanyRules.java
@@ -1,11 +1,8 @@
 package com.bzncrsrtc.kurumsalmuvekkil.rules;
 
-import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.bzncrsrtc.kurumsalmuvekkil.exceptions.CompanyNotFoundException;
@@ -15,46 +12,39 @@ import com.bzncrsrtc.kurumsalmuvekkil.repositories.CompanyRepository;
 @Component
 public class CompanyRules {
 	
-	private final MessageSource messageSource;
 	private final CompanyRepository companyRepository;
 	
-	public CompanyRules(MessageSource messageSource, CompanyRepository companyRepository) {
-		this.messageSource = messageSource;
+	public CompanyRules(CompanyRepository companyRepository) {
 		this.companyRepository = companyRepository;
 	}
 
 	public void isNull(Optional<Company> company) {
 		if(company.isEmpty()) {
-			Locale locale = LocaleContextHolder.getLocale();
-			throw new CompanyNotFoundException(messageSource.getMessage("company.not.found.message", null, "An error occured", locale));
+			throw new CompanyNotFoundException("message");
 		}
 	}
 	
 	public void isNull(Company company) {
 		if(company == null) {
-			Locale locale = LocaleContextHolder.getLocale();
-			throw new CompanyNotFoundException(messageSource.getMessage("company.not.found.message", null, "An error occured", locale));
+			throw new CompanyNotFoundException("message");
 		}
 	}
 
 	public void isThere(UUID id) {
 		if(id == null || companyRepository.existsById(id)) {
-			Locale locale = LocaleContextHolder.getLocale();
-			throw new CompanyNotFoundException(messageSource.getMessage("company.not.found.message", null, "An error occured", locale));
+			throw new CompanyNotFoundException("message");
 		}
 	}
 	
 	public void isDeleted(Company company) {
 		if(company.isDeleted()) {
-			Locale locale = LocaleContextHolder.getLocale();
-			throw new CompanyNotFoundException(messageSource.getMessage("company.not.found.message", null, "An error occured", locale));
+			throw new CompanyNotFoundException("message");
 		}
 	}
 	
 	public void isPassive(Company company) {
 		if(!company.isActive()) {
-			Locale locale = LocaleContextHolder.getLocale();
-			throw new CompanyNotFoundException(messageSource.getMessage("company.not.found.message", null, "An error occured", locale));
+			throw new CompanyNotFoundException("message");
 		}
 	}
 	
