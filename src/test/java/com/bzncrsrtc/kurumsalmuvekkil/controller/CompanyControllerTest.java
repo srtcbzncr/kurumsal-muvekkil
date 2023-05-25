86ce7d1df40ef0f49e8527be30cf9acc81bda5e9
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-05-25 23:57:19 +0300

    Started CompanyControllerTest

--- src/test/java/com/bzncrsrtc/kurumsalmuvekkil/controller/CompanyControllerTest.java
+++ src/test/java/com/bzncrsrtc/kurumsalmuvekkil/controller/CompanyControllerTest.java
@@ -1,11 +1,14 @@
 package com.bzncrsrtc.kurumsalmuvekkil.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.bzncrsrtc.kurumsalmuvekkil.controllers.CompanyController;
@@ -27,7 +30,12 @@ class CompanyControllerTest {
 	
 	@MockBean
 	CompanyService companyService;
-
 	
+	@Test
+	public void findAllTest_WithEnAcceptHeaderLanguage_ShouldReturn200() throws Exception {
+		mockMvc.perform(get("/company")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 
 }
