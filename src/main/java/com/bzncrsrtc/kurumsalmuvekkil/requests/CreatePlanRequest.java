b5c497c5f7386cbf397a60bfca8d869155fb6c5d
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-08-09 17:20:22 +0300

    CreatePlanRequest min validation added

--- src/main/java/com/bzncrsrtc/kurumsalmuvekkil/requests/CreatePlanRequest.java
+++ src/main/java/com/bzncrsrtc/kurumsalmuvekkil/requests/CreatePlanRequest.java
@@ -2,6 +2,7 @@ package com.bzncrsrtc.kurumsalmuvekkil.requests;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -16,18 +17,23 @@ public class CreatePlanRequest {
 	private String description;
 	
 	@NotNull(message="{monthlyPrice.notNull}")
+	@Min(value=0, message="{monthlyPrice.minError}")
 	private BigDecimal monthlyPrice;
 	
 	@NotNull(message="{annualPrice.notNull}")
+	@Min(value=0, message="{annualPrice.minError}")
 	private BigDecimal annualPrice;
 	
 	@NotNull(message="{clientQuota.notNull}")
+	@Min(value=0, message="{clientQuota.minError}")
 	private Integer clientQuota;
 	
 	@NotNull(message="{lawyerQuota.notNull}")
+	@Min(value=0, message="{lawyerQuota.minError}")
 	private Integer lawyerQuota;
 	
 	@NotNull(message="{fileQuotaPerClient.notNull}")
+	@Min(value=0, message="{fileQuotaPerClient.minError}")
 	private Integer fileQuotaPerClient;
 	
 }
--- target/generated-sources/annotations/com/bzncrsrtc/kurumsalmuvekkil/mappers/ResponseMapperImpl.java
+++ target/generated-sources/annotations/com/bzncrsrtc/kurumsalmuvekkil/mappers/ResponseMapperImpl.java
@@ -31,7 +31,7 @@ import org.springframework.stereotype.Component;
 /*
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-04T16:18:29+0300",
+    date = "2023-08-09T16:07:35+0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230218-1114, environment: Java 17.0.6 (Eclipse Adoptium)"
 )
 */
