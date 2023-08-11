5f05d70ca2a32a706da41e3f9d917b26103c452e
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-08-11 17:18:18 +0300

    LawyerControllerTest updated

--- src/test/java/com/bzncrsrtc/kurumsalmuvekkil/integration/LawyerControllerTest.java
+++ src/test/java/com/bzncrsrtc/kurumsalmuvekkil/integration/LawyerControllerTest.java
@@ -1,10 +1,16 @@
 package com.bzncrsrtc.kurumsalmuvekkil.integration;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.bzncrsrtc.kurumsalmuvekkil.repositories.LawyerRepository;
+import com.bzncrsrtc.kurumsalmuvekkil.repositories.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -13,4 +19,20 @@ import org.springframework.test.context.jdbc.Sql;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class LawyerControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private LawyerRepository lawyerRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	
+	@BeforeEach
+	void setup() {
+		
+	}
+	
 }
