3c147ab6667c00aa2a7f176bb63ef60a1c08d96a
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-08-13 16:35:50 +0300

    title field deleted from Lawyer model

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -31,8 +31,8 @@ VALUES('8056e4bc-1060-430b-912a-90126876b06d', 'client@gmail.com', 'client', '12
 INSERT INTO companies(id, name, active, deleted) 
 VALUES('ace5ae43-6060-49c2-9f74-0bfdcf8a7d9d', 'Company', 'true', 'false');
 
-INSERT INTO lawyers(id, company_id, user_id, title, identification_number, first_name, last_name, phone, active, deleted) 
-VALUES('8056e4bc-1060-430b-912a-90126876b06d', 'ace5ae43-6060-49c2-9f74-0bfdcf8a7d9d', '5af5b243-31fb-4ae2-a342-74c14ed16104', 'Avukat', '33274102316', 'Ali', 'Akın', '05348540650', 'true', 'false');
+INSERT INTO lawyers(id, company_id, user_id, identification_number, first_name, last_name, phone, active, deleted) 
+VALUES('8056e4bc-1060-430b-912a-90126876b06d', 'ace5ae43-6060-49c2-9f74-0bfdcf8a7d9d', '5af5b243-31fb-4ae2-a342-74c14ed16104', '33274102316', 'Ali', 'Akın', '05348540650', 'true', 'false');
 
 INSERT INTO clients(id, user_id, identification_number, first_name, last_name, phone, active, deleted) 
 VALUES('1f16438f-7f27-4056-a20c-ea71bca9d6a5', '8056e4bc-1060-430b-912a-90126876b06d', '33274102316', 'Ahmet', 'Yaşa', '05435698456', 'true', 'false');
